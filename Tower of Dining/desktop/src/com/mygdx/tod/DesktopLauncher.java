e3c663e8bc2c7c31c5f3637d04e386e2060d8139
Ali Deniz Sözer <113902419+alideno@users.noreply.github.com>
2023-05-10 14:28:55 +0300

    Add files via upload

---
+++ Tower of Dining/desktop/src/com/mygdx/tod/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.mygdx.tod;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.mygdx.tod.TowerOfDining;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Tower Of Dining");
+		new Lwjgl3Application(new TowerOfDining(), config);
+	}
+}
