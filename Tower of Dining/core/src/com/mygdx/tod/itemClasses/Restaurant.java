f1409b1f948d6a77f237a213c8a1cf6e1be2d6db
Deniz Şahin <denizsahin567@gmail.com>
2023-05-23 13:21:42 +0300

    Restaurant Class Basics
    The first implementation of the restaurant class with some basic methods, more to be added later based on need.

---
+++ Tower of Dining/core/src/com/mygdx/tod/itemClasses/Restaurant.java
@@ -0,0 +1,74 @@
+package com.mygdx.tod.itemClasses;
+/**
+ * This class represents a restaurant and manages the functionality of a restaurant.
+ * 
+ * @author Deniz Şahin
+ */
+public class Restaurant {
+    
+    //defining the food related variables
+    private Food[] foods; //the foods this restaurant sells
+    private int addedFoods; //the number of foods added
+    
+    //defining the restaurant related variables
+    private boolean isOpen; //is restaurant open
+
+    //constructor
+    Restaurant()
+    {
+        foods = new Food[3];
+        addedFoods = 0;
+        isOpen = false;    
+    }
+
+    /**
+     * This method adds the specified food to the foods array of this restaurant
+     * @return the number of foods added, -1 if all food slots are full
+     */
+    public int addFoodItem(Food food)
+    {
+        if(addedFoods < 3)
+        {
+            this.foods[addedFoods] = food;
+            this.addedFoods++;
+            return addedFoods;
+        }
+        return -1;
+    }
+
+    /**
+     * This method turns the restaurant status to open
+     * Invoke when restaurant is bought
+     */
+    public void openRestaurant()
+    {
+        this.isOpen = true;
+    }
+
+    /**
+     * This method returns the opennes status of the restaurant
+     * @return true if restaurant is open
+     */
+    public boolean isOpen()
+    {
+        return this.isOpen;
+    }
+
+    /**
+     * This method calculates the end of day income and reduces the food used up
+     * @return income this restaurant provided on this day
+     */
+    public int endDay()
+    {
+        int earning = 0;
+        if(this.isOpen)
+        {
+            for(int i = 0; i < 3; i++)
+            {
+                int customers = foods[i].calculateCustomers();
+                earning += (this.foods[i].getSellPrice() * customers);
+            }
+        }
+        return earning;
+    }
+}
