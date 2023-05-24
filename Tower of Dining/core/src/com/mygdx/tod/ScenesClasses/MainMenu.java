e816c4557cea2928436e1ae099a70d5aa31c8ca3
Eren Berk Eraslan <berk.eraslan@ug.bilkent.edu.tr>
2023-05-24 14:58:34 +0300

    screens added

--- Tower of Dining/core/src/com/mygdx/tod/ScenesClasses/MainMenu.java
+++ Tower of Dining/core/src/com/mygdx/tod/ScenesClasses/MainMenu.java
@@ -1,5 +1,26 @@
 package com.mygdx.tod.ScenesClasses;
 
-public class MainMenu {
-    
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.mygdx.tod.TowerOfDining;
+
+public class MainMenu extends ScreenAdapter {
+    Texture menuImg;
+    TowerOfDining game;
+
+    public MainMenu(TowerOfDining tod){
+        game = tod;
+        menuImg =  new Texture("title screen.png");
+        
+    }
+
+   
+    @Override
+    public void render(float delta) {
+		ScreenUtils.clear(1, 0, 0, 1);
+		game.batch.begin();
+		game.batch.draw(menuImg, 0, 0);
+		game.batch.end();
+    }
 }
--- Tower of Dining/core/src/com/mygdx/tod/TowerOfDining.java
+++ Tower of Dining/core/src/com/mygdx/tod/TowerOfDining.java
@@ -1,28 +1,26 @@
 package com.mygdx.tod;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Screen;
+
+import java.util.Stack;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.mygdx.tod.ScenesClasses.MainMenu;
 
-public class TowerOfDining extends ApplicationAdapter {
-	SpriteBatch batch;
+public class TowerOfDining extends Game {
+	public SpriteBatch batch;
 	Texture img;
+	public Stack<Screen> screens = new Stack<>();
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("title screen.png");
+		screens.push(new MainMenu(this));
+		setScreen(screens.get(0));
+		
 	}
 
-	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
 	@Override
 	public void dispose () {
 		batch.dispose();
