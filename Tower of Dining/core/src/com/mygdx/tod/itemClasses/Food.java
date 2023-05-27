ce0a87f50397379313f1f95dabae9c2d82a49e30
Ali Deniz <alidenizsozer@gmail.com>
2023-05-27 14:44:11 +0300

    started the food class

--- Tower of Dining/core/src/com/mygdx/tod/itemClasses/Food.java
+++ Tower of Dining/core/src/com/mygdx/tod/itemClasses/Food.java
@@ -1,5 +1,23 @@
 package com.mygdx.tod.itemClasses;
 
 public class Food {
+    private int sellPrice, basePrice, currentStock;
+    private final int maxStock = 40;
+
+
+    public Food(int basePrice){
+        this.basePrice = basePrice;
+        currentStock = maxStock;
+        sellPrice = basePrice;
+    }
+    public void setPrice(int newPrice){
+        sellPrice = newPrice;
+    }
+    public int getSellPrice() {
+        return sellPrice;
+    }
+    public int getBasePrice(){
+        return basePrice;
+    }
     
 }
