b6f4e1c3b702d3cb1d832e4b48bd01c123fa33e6
Eren Berk Eraslan <berk.eraslan@ug.bilkent.edu.tr>
2023-05-29 21:52:49 +0300

    String name added to Food class

--- Tower of Dining/core/src/com/mygdx/tod/itemClasses/Food.java
+++ Tower of Dining/core/src/com/mygdx/tod/itemClasses/Food.java
@@ -3,24 +3,33 @@ package com.mygdx.tod.itemClasses;
 public class Food {
     private int sellPrice, basePrice, currentStock;
     private final int maxStock = 40;
+    private String name;
 
-
-    public Food(int basePrice){
+    public Food(int basePrice, String name) {
+        this.name = name;
         this.basePrice = basePrice;
         currentStock = maxStock;
         sellPrice = basePrice;
     }
-    public void setPrice(int newPrice){
+
+    public String getName() {
+        return name;
+    }
+
+    public void setPrice(int newPrice) {
         sellPrice = newPrice;
     }
+
     public int getSellPrice() {
         return sellPrice;
     }
-    public int getBasePrice(){
+
+    public int getBasePrice() {
         return basePrice;
     }
+
     public int calculateCustomers() {
         return 0;
     }
-    
+
 }
