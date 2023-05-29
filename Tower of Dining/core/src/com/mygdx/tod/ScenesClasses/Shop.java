fc8673a72fda3471743c636be624a2f14ba6f52a
Eren Berk Eraslan <berk.eraslan@ug.bilkent.edu.tr>
2023-05-29 22:01:37 +0300

    goBack implemented in Shop screen

--- Tower of Dining/core/src/com/mygdx/tod/ScenesClasses/Shop.java
+++ Tower of Dining/core/src/com/mygdx/tod/ScenesClasses/Shop.java
@@ -1,6 +1,8 @@
 package com.mygdx.tod.ScenesClasses;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Cursor;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
@@ -10,32 +12,32 @@ import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.Button.ButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.mygdx.tod.TowerOfDining;
 
-public class Shop extends ScreenAdapter{
+public class Shop extends ScreenAdapter {
     Texture menuImg;
     private TowerOfDining game;
+    private TowerMenu towerMenu;
     private Button nextButton, previousButton, orderButton, backButton;
     private int currentShop;
     int[] foodCount;
     private Stage stage;
-    
 
-
-    public Shop(TowerOfDining game, int currentShop){//for towermenu class
+    public Shop(TowerOfDining game, int currentShopx, TowerMenu towerMenu) {// for towermenu class
         TextureRegionDrawable upDrawable = new TextureRegionDrawable(new TextureRegion(new Texture("dayend.png")));
         TextureRegionDrawable downDrawable = new TextureRegionDrawable(new TextureRegion(new Texture("dayend.png")));
         ButtonStyle buttonStyle = new ButtonStyle();
         buttonStyle.up = upDrawable;
         buttonStyle.down = downDrawable;
-
         this.game = game;
+        this.towerMenu = towerMenu;
         this.currentShop = currentShop;
-        String temp = "shop menu" + (currentShop+1) + ".png";
-        menuImg =  new Texture(temp);
+        String temp = "shop menu" + (currentShop + 1) + ".png";
+        menuImg = new Texture(temp);
         foodCount = new int[24];
         for (int i = 0; i < foodCount.length; i++) {
             foodCount[i] = 0;
@@ -44,12 +46,12 @@ public class Shop extends ScreenAdapter{
 
         nextButton = new Button(buttonStyle);
         nextButton.setColor(1f, 1f, 1f, 0f);
-        nextButton.setBounds(1500, 196, 110,390);
+        nextButton.setBounds(1500, 196, 110, 390);
         nextButton.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
                 // TODO
-                
+
             }
 
         });
@@ -69,24 +71,54 @@ public class Shop extends ScreenAdapter{
 
         previousButton = new Button(buttonStyle);
         previousButton.setColor(1f, 1f, 1f, 0f);
-        previousButton.setBounds(1500, 305, 110,390);
+        previousButton.setBounds(1500, 305, 110, 390);
 
         backButton = new Button(buttonStyle);
-        backButton.setColor(1f, 1f, 1f, 0f);
-        backButton.setBounds(1152, 58, 100,750);
-
-
+        backButton.setBounds(1100, 920, 750, 120);
+        backButton.setColor(Color.BLACK);
         stage.addActor(nextButton);
         stage.addActor(previousButton);
         stage.addActor(backButton);
-
+        goBack();
     }
-    public Shop(TowerOfDining game, int currentShop, int[] foodCount){ //ONLY USE IN THIS CLASS
-        Shop shop = new Shop(game, currentShop);
+
+    public void goBack() {
+        backButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                towerMenu.render(1);
+
+                game.closeScreen();
+
+            }
+
+        });
+
+        backButton.addListener(new InputListener() {
+            @Override
+            public void enter(InputEvent event, float x, float y, int pointer, Actor fromActor) {
+                // Set the cursor to Hand when the mouse enters the button
+                Gdx.graphics.setSystemCursor(Cursor.SystemCursor.Hand);
+            }
+
+            @Override
+            public void exit(InputEvent event, float x, float y, int pointer, Actor toActor) {
+                // Restore the default cursor when the mouse exits the button
+                Gdx.graphics.setSystemCursor(Cursor.SystemCursor.Arrow);
+            }
+        });
+        render(1);
+    }
+
+    public Shop(TowerOfDining game, int currentShop, int[] foodCount) { // ONLY USE IN THIS CLASS
+        Shop shop = new Shop(game, currentShop, towerMenu);
         shop.foodCount = foodCount;
     }
 
-
+    @Override
+    public void show() {
+        Gdx.input.setInputProcessor(stage);
+    }
 
     @Override
     public void render(float delta) {
@@ -103,7 +135,5 @@ public class Shop extends ScreenAdapter{
     public void dispose() {
         stage.dispose();
     }
-
-
 
 }
